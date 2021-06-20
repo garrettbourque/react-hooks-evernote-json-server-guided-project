c9e14f259a57f865ca4020893c2ae4a97917ac38
garrettBourque <bourque.garrett@gmail.com>
2021-06-20 18:58:59 -0500

    serach works

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import NoteItem from "./NoteItem";
 
 function NoteList({ notes,setNote, foundNote }) {
-  console.log(foundNote)
+  //console.log(foundNote)
   return (
     <ul>
       {foundNote.map(notes => <NoteItem notes={notes} setNote={setNote}/>
