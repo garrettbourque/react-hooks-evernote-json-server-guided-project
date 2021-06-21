e93577b2111f00bd6006bd4cc5b15a20c08b312b
garrettBourque <bourque.garrett@gmail.com>
2021-06-20 21:00:15 -0500

    add default note works

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -18,13 +18,33 @@ function NoteContainer() {
   const foundNote = notes.filter((fnote)=>
     fnote.title.toLowerCase().includes(fNote.toLowerCase())
     )
-    
+  const newNote=()=>{
+    const newNotes ={
+      id:"",
+      userId:"1",
+      title:"default",
+      body:"empty"
+    }
+    setNotes([...notes,newNotes])
+    //console.log(newNotes)
+    fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newNotes),
+    })
+      .then((r) => r.json())
+     // .then(console.log(new newNotes));
+  }
+
+  
 
   return (
     <>
       <Search fNote={fNote} setFNote={setFNote}/>
       <div className="container">
-        <Sidebar notes ={notes} setNote={setNote} foundNote={foundNote}/>
+        <Sidebar notes ={notes} setNote={setNote} foundNote={foundNote} newNote={newNote}/>
         <Content notes ={note}/>
       </div>
     </>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NoteList from "./NoteList";
 
-function Sidebar({ notes,setNote,foundNote }) {
+function Sidebar({ notes,setNote,foundNote,newNote }) {
+
 
  // console.log(foundNote)
   return (
     <div className="master-detail-element sidebar">
       <NoteList notes={notes} setNote={setNote}foundNote={foundNote}/>
-      <button>New</button>
+      <button onClick={newNote}>New</button>
     </div>
   );
 }
